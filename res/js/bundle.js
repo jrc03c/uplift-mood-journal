70eb7e6653eb0bd9c243606b89d5132afc9f4115
jrc03c <jrc03c@pm.me>
2021-06-11 19:22:11 -0500

    ...

--- res/js/bundle.js
+++ res/js/bundle.js
@@ -34,26 +34,26 @@ module.exports = Vue.component("slider", {
   },
 
   template: /*html*/ `
-  <div class="slider-answer-widget">
-    <div class="slider slider-horizontal">
-      <div class="slider-track">
-        <div class="slider-track-low" style="left: 0px; width: 0%;"></div>
-        <div class="slider-selection" style="left: 0%; width: 38%;"></div>
-        <div class="slider-track-high" style="right: 0px; width: 62%;"></div>
-      </div>
+    <div class="slider-answer-widget">
+      <div class="slider slider-horizontal">
+        <div class="slider-track">
+          <div class="slider-track-low" style="left: 0px; width: 0%;"></div>
+          <div class="slider-selection" style="left: 0%; width: 38%;"></div>
+          <div class="slider-track-high" style="right: 0px; width: 62%;"></div>
+        </div>
 
-      <div class="tooltip tooltip-main top in" role="presentation" style="left: 38%;">
-        <div class="tooltip-arrow"></div>
-        <div class="tooltip-inner">38</div>
-      </div>
+        <div class="tooltip tooltip-main top in" role="presentation" style="left: 38%;">
+          <div class="tooltip-arrow"></div>
+          <div class="tooltip-inner">38</div>
+        </div>
 
-      <div class="slider-handle min-slider-handle round" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 38%;" aria-valuenow="38" tabindex="0">
-      </div>
+        <div class="slider-handle min-slider-handle round" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 38%;" aria-valuenow="38" tabindex="0">
+        </div>
 
-      <div class="slider-handle max-slider-handle round hide" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 0%;" aria-valuenow="0" tabindex="0">
+        <div class="slider-handle max-slider-handle round hide" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 0%;" aria-valuenow="0" tabindex="0">
+        </div>
       </div>
     </div>
-  </div>
   `,
 })
 
