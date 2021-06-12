da82bb7f5a9a0cc3d413f8ccb208b4fd0d3dcbd1
jrc03c <jrc03c@pm.me>
2021-06-11 19:30:00 -0500

    ...

--- res/js/bundle.js
+++ res/js/bundle.js
@@ -38,23 +38,58 @@ module.exports = Vue.component("slider", {
       <div class="slider slider-horizontal">
         <div class="slider-track">
           <div class="slider-track-low" style="left: 0px; width: 0%;"></div>
-          <div class="slider-selection" style="left: 0%; width: 38%;"></div>
-          <div class="slider-track-high" style="right: 0px; width: 62%;"></div>
+          <div class="slider-selection" :style="sliderSelectionStyle"></div>
+          <div class="slider-track-high" :style="sliderTrackHighStyle"></div>
         </div>
 
-        <div class="tooltip tooltip-main top in" role="presentation" style="left: 38%;">
+        <div class="tooltip tooltip-main top in" role="presentation" :style="tooltipAndHandleStyle">
           <div class="tooltip-arrow"></div>
-          <div class="tooltip-inner">38</div>
+          <div class="tooltip-inner">{{ value }}</div>
         </div>
 
-        <div class="slider-handle min-slider-handle round" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 38%;" aria-valuenow="38" tabindex="0">
+        <div class="slider-handle min-slider-handle round" role="slider" :aria-valuemin="min" :aria-valuemax="max" :style="tooltipAndHandleStyle" :aria-valuenow="value" tabindex="0">
         </div>
 
-        <div class="slider-handle max-slider-handle round hide" role="slider" aria-valuemin="0" aria-valuemax="100" style="left: 0%;" aria-valuenow="0" tabindex="0">
+        <div class="slider-handle max-slider-handle round hide" role="slider" :aria-valuemin="min" :aria-valuemax="max" style="tooltipAndHandleStyle" :aria-valuenow="value" tabindex="0">
         </div>
       </div>
     </div>
   `,
+
+  data: function () {
+    return {
+      value: 50,
+    }
+  },
+
+  computed: {
+    percent: function () {
+      const self = this
+      return (100 * (self.value - self.min)) / (self.max - self.min)
+    },
+
+    sliderSelectionStyle: function () {
+      const self = this
+      return `left: 0%; width: ${self.percent}%;`
+    },
+
+    sliderTrackHighStyle: function () {
+      const self = this
+      return `right: 0px; width: ${100 - self.percent}%;`
+    },
+
+    tooltipAndHandleStyle: function () {
+      const self = this
+      return `left: ${self.percent}%;`
+    },
+  },
+
+  mounted: function () {
+    const self = this
+    self.value = self.startValue
+  },
+
+  beforeDestroy: function () {},
 })
 
 },{"vue/dist/vue.min.js":1}],3:[function(require,module,exports){
